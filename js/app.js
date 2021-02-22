65611d4c8cefc5bf8e0a31d90043617df32001f3
osmankbk <mackiekbk@yahoo.com>
2021-02-22 17:42:17 -0500

    Add real-time search filter functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
+const searchInput = document.querySelector('#search');
+const images = document.querySelectorAll('img');
+
 
 
 window.addEventListener('load', () => {
     baguetteBox.run('.gallery');
+});
+
+searchInput.addEventListener('keydown', (e) => {
+    let input = e.target.value.toLowerCase();
+    images.forEach(image => {
+        let imageName = image.alt.toLocaleLowerCase();
+        if(imageName.includes(input)) {
+            image.style.display = 'inline-block';
+        } else {
+            image.style.display = 'none';
+        }
+    })
 })
+

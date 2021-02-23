7bd037bb0d038bec0438001695cfe27b1ef110ec
osmankbk <mackiekbk@yahoo.com>
2021-02-22 23:00:26 -0500

    Change getAttribute() argument from 'caption' to data-caption

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//This event runs the image ducal on load from the baguettebox file.
+window.addEventListener('load', () => {
+	baguetteBox.run('.gallery');
+});
+
 //Save the searh input to searhInput constant
 const searchInput = document.querySelector('#search');
 //A function that takes a user's input as argument, interate through the anchor elements, & display the child(img)
@@ -7,7 +12,7 @@ const realTimeSearch = (input) => {
 	const anchors = document.querySelectorAll('a');
 	anchors.forEach(anchor => {
 		//Save the caption attribute of each anchor to the caption variable.
-		let caption = anchor.getAttribute('caption').toLocaleLowerCase();
+		let caption = anchor.getAttribute('data-caption').toLocaleLowerCase();
 		//Save the anchor's child(image) to the image variable.
 		let image = anchor.firstElementChild;
 		//This condition checks the anchor's caption for any of the user's inputs letter, & display its child if found.
@@ -19,10 +24,7 @@ const realTimeSearch = (input) => {
 		}
 	});
 }
-//This event runs the image ducal on load from the baguettebox file.
-window.addEventListener('load', () => {
-	baguetteBox.run('.gallery');
-});
+
 //This event implement the real-time searh on when keys are pushed.
 searchInput.addEventListener('keyup', (e) => {
 	//Saved value of the users input to the input variable, and feed it, as argument to the realTimeSearch() function.
